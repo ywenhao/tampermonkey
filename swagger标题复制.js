929243de05c108b151d795c2a39683c56a11c2df
ywenhao <37856060+ywenhao@users.noreply.github.com>
2023-06-18 19:00:13 +0800

    feat: swagger 更新

--- swagger标题复制.js
+++ swagger标题复制.js
@@ -14,7 +14,7 @@
     'use strict';
     function handleClick(e) {
         e.stopPropagation()
-        var text = e.target.innerText;
+        var text = e.target.dataset.path || e.target.innerText;
         var copyDom = document.createElement('textarea');
         copyDom.setAttribute('readonly', 'readonly');
         copyDom.style.position = 'absolute';
@@ -28,10 +28,16 @@
 
     setTimeout(() => {
         var els = Array.from(document.querySelectorAll('.opblock-summary-description'));
+        var paths = Array.from(document.querySelectorAll('.opblock-summary-path'));
         els.forEach(el => {
             el.style.marginRight = 'auto';
             el.style.maxWidth = 'max-content';
             el.style.cursor = 'copy';
+            el.addEventListener('click', handleClick);
+        })
+        paths.forEach(el => {
+            var a = el.querySelector('a');
+            a.style.cursor = 'copy';
             el.addEventListener('click', handleClick);
         })
     }, 500)
