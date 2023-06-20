da2d51f2fafa670b86e957e711cfdde978e58b6a
actions.win <actions.win@foxmail.com>
2023-06-20 09:19:07 +0800

    feat: generateFunctionName 添加条件

--- swagger快速生成api.js
+++ swagger快速生成api.js
@@ -58,6 +58,8 @@
       .split('/')
       .filter((item) => item && !item.includes('{') && !/^v\d+$/.test(item))
       .flatMap((v) => v.split('-'))
+      .flatMap((v) => v.split('_'))
+      .filter(Boolean)
       .map((str) => str.replace(str[0], str[0].toUpperCase()))
     name += titles.join('')
     var condition = apiTitle.includes('列表')
