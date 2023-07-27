e92a1d395f4cebf6fa04904f1e63c70a01caf51c
actions.win <actions.win@foxmail.com>
2023-07-27 17:36:08 +0800

    feat: gitlab 修正地址

---
+++ gitlab.js
@@ -0,0 +1,19 @@
+// ==UserScript==
+// @name         gitlab修正clone地址
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  try to take over the world!
+// @author       You
+// @match        http://192.168.2.253/*
+// @match        http://gitlab.test.com/*
+// @icon         https://i.vimeocdn.com/portrait/72670399_60x60?subrect=69%2C60%2C307%2C298&r=cover
+// @grant        none
+// ==/UserScript==
+
+;(function () {
+  'use strict'
+  var sshInput = document.querySelector('input#ssh_project_clone')
+  if (!sshInput) return
+  var value = sshInput.value.replace('com:', 'com:2223/')
+  sshInput.value = `ssh://${value}`
+})()
